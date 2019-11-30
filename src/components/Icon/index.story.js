10540df4fe21a69b5b41ad34860e65e1a5dc1304
Joshua Crass <joshua.crass@gmail.com>
2019-11-30 11:12:20 -0800

    fixed Icon file extention

--- src/components/Icon/index.story.js
+++ src/components/Icon/index.story.js
@@ -4,4 +4,4 @@ import { Icon } from '../index';
 
 export default { title: 'Icons' };
 
-export const icon = () => <Icon className="testing">apps</Icon>;
+export const icon = () => <Icon className="testing">test</Icon>;
